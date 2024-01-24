c4bd4f9ccd1b6de6d30484d4ee13b371dae217f2
sandraloisio <saroal874@gmail.com>
2024-01-24 09:39:27 +0100

    Creo variables y método main

---
+++ src/TresEnRaya.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class TresEnRaya {
+    private static char tablero[] = new char[9];
+    private static int victorias=0, derrotas=0;
+    private static final char charUsuario='x', charMaquina='o';
+
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+    }
+}
